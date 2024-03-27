101d1b7a03cfb28a4301ac18021841dcf8d050b9
kang <guswjd9994@duksung.ac.kr>
2024-03-27 21:02:02 +0900

    [feat] 회원가입 유효성 검사

--- Introduction/app/src/main/java/com/example/introduction/SignUpActivity.kt
+++ Introduction/app/src/main/java/com/example/introduction/SignUpActivity.kt
@@ -3,58 +3,116 @@ package com.example.introduction
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
 class SignUpActivity : AppCompatActivity() {
+    // var은 lateinit
     private lateinit var viewModel: SignUpViewModel
+    // val은 lazy
+    private val nameEt: EditText by lazy {
+        findViewById(R.id.name_ET)
+    }
+
+    private val idEt: EditText by lazy {
+        findViewById(R.id.id_ET)
+    }
+
+    private val pwdEt: EditText by lazy {
+        findViewById(R.id.pwd_ET)
+    }
+
+    private val pwdcheckEt: EditText by lazy {
+        findViewById(R.id.pwd_check_ET)
+    }
+
+    private val pwd_warningTV: TextView by lazy {
+        findViewById(R.id.pwd_warning_TV)
+    }
+
+    private val pwd_check_warningTV: TextView by lazy {
+        findViewById(R.id.pwd_check_warning_TV)
+    }
+
+    private val signupBtn: Button by lazy {
+        findViewById(R.id.signup_btn)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        // ViewModelProvider를 통해 ViewModel 가져오기
+        // LifeCycle을 가진 owner를 넣는다(this==현재 Activity)
+        // get()에 가져오고 싶은 ViewModel클래스를 넣으면 완료
         viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
-
-        val nameEt: EditText = findViewById(R.id.name_ET)
-        val idEt: EditText = findViewById(R.id.id_ET)
-        val pwdEt: EditText = findViewById(R.id.pwd_ET)
-        val signupBtn: Button = findViewById(R.id.signup_btn)
-
-        // LiveData를 관찰하여 변경 사항을 처리
-        viewModel.name.observe(this, Observer { name ->
-            nameEt.setText(name)
+        viewModel.password.observe(this, Observer { inputName->
+            nameEt.setText(inputName)
         })
 
-        viewModel.id.observe(this, Observer { id ->
-            idEt.setText(id)
-        })
-
-        viewModel.password.observe(this, Observer { password ->
-            pwdEt.setText(password)
-        })
-
-        signupBtn.setOnClickListener {
-            val name = nameEt.text.toString()
-            val id = idEt.text.toString()
-            val pwd = pwdEt.text.toString()
-
-            viewModel.setName(name)
-            viewModel.setId(id)
-            viewModel.setPassword(pwd)
-
-            if (viewModel.isValidInput()) {
-                Toast.makeText(this@SignUpActivity, "회원가입 성공\n이름: $name\n아이디: $id\n비밀번호: $pwd", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
-                intent.putExtra("id", id)
-                intent.putExtra("pwd", pwd)
-                setResult(RESULT_OK, intent)
-                finish()
-            } else {
-                Toast.makeText(this@SignUpActivity, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+        pwdEt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (!viewModel.isStrongPassword(pwdEt.text.toString())){
+                    pwd_warningTV.visibility = View.VISIBLE
+                    signupBtn.isEnabled = false
+                } else {
+                    pwd_warningTV.visibility = View.GONE
+                }
             }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        pwdcheckEt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (!checkPasswordMatch()){
+                    pwd_check_warningTV.visibility = View.VISIBLE
+
+                } else {
+                    pwd_check_warningTV.visibility = View.GONE
+                    signupBtn.isEnabled = true
+                }
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        signupBtn.isEnabled = false
+        signupBtn.setOnClickListener {
+            signUpButtonClick()
         }
+    }
+
+    private fun signUpButtonClick() {
+        val name = nameEt.text.toString()
+        val id = idEt.text.toString()
+        val pwd = pwdEt.text.toString()
+
+        if (name.isNotEmpty() && id.isNotEmpty() && pwd.isNotEmpty()) {
+            viewModel.setUserValue(name, id, pwd)
+            Toast.makeText(this@SignUpActivity, "회원가입 성공\n이름: $name\n아이디: $id\n비밀번호: $pwd", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+            intent.putExtra("id", id)
+            intent.putExtra("pwd", pwd)
+            // 이전 액티비티로 돌아가기 전에 현재 액티비티에서 처리한 결과를 담은 인텐트를 설정
+            setResult(RESULT_OK, intent)
+            finish()
+        } else {
+            Toast.makeText(this@SignUpActivity, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkPasswordMatch(): Boolean {
+        val password = pwdEt.text.toString()
+        val passwordCheck = pwdcheckEt.text.toString()
+
+        return password == passwordCheck
     }
 }
--- Introduction/app/src/main/java/com/example/introduction/SignUpViewModel.kt
+++ Introduction/app/src/main/java/com/example/introduction/SignUpViewModel.kt
@@ -5,30 +5,31 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SignUpViewModel : ViewModel() {
+    // 뷰모델 내부에서 설정하는 자료형은 뮤터블로 해서 변경가능하도록 설정
     private val _name = MutableLiveData<String>()
-    val name: LiveData<String> = _name
+    // 변경되지 않는 데이터를 가져올 떄는 '_'없이 설정
+    // 공개적으로 가져오는 변수는 private X, 외부에서도 접근가능하게 설정
+    // 값을 직접 LiveData에 접근 X, 뷰모델을 통해 가져오게 설정
+    val name: LiveData<String>
+        get() = _name
 
     private val _id = MutableLiveData<String>()
-    val id: LiveData<String> = _id
+    val id: LiveData<String>
+        get() = _id
 
     private val _password = MutableLiveData<String>()
-    val password: LiveData<String> = _password
+    val password: LiveData<String>
+        get() =_password
 
-    fun setName(name: String) {
+    fun isStrongPassword(password: String): Boolean {
+        // 대문자, 소문자, 특수문자, 숫자를 포함하는 정규식
+        val pattern = Regex("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*+=])(?=\\S+$).{8,15}$")
+        return pattern.matches(password)
+    }
+
+    fun setUserValue(name: String, id: String, password: String) {
         _name.value = name
-    }
-
-    fun setId(id: String) {
         _id.value = id
-    }
-
-    fun setPassword(password: String) {
         _password.value = password
-    }
-
-    fun isValidInput(): Boolean {
-        return _name.value?.isNotEmpty() ?: false &&
-                _id.value?.isNotEmpty() ?: false &&
-                _password.value?.isNotEmpty() ?: false
     }
 }
