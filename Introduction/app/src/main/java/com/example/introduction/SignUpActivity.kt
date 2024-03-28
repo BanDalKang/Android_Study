c1aaf17fb5e2a5153e4029d58ba1d63a27487c1e
kang <guswjd9994@duksung.ac.kr>
2024-03-28 21:00:50 +0900

    [refactor] SignUpViewModel 수정

--- Introduction/app/src/main/java/com/example/introduction/SignUpActivity.kt
+++ Introduction/app/src/main/java/com/example/introduction/SignUpActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
@@ -53,66 +54,64 @@ class SignUpActivity : AppCompatActivity() {
         // LifeCycle을 가진 owner를 넣는다(this==현재 Activity)
         // get()에 가져오고 싶은 ViewModel클래스를 넣으면 완료
         viewModel = ViewModelProvider(this).get(SignUpViewModel::class.java)
-        viewModel.password.observe(this, Observer { inputName->
-            nameEt.setText(inputName)
-        })
 
-        pwdEt.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                if (!viewModel.isStrongPassword(pwdEt.text.toString())){
-                    pwd_warningTV.visibility = View.VISIBLE
-                    signupBtn.isEnabled = false
-                } else {
-                    pwd_warningTV.visibility = View.GONE
-                }
+        viewModel.isPasswordValid.observe(this) { isPasswordValid ->
+            pwd_warningTV.visibility = if (isPasswordValid) {
+                View.GONE // 비밀번호가 강력한 경우 텍스트뷰 숨기기
+            } else {
+                View.VISIBLE // 비밀번호가 강력하지 않은 경우 텍스트뷰 표시
             }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
+        }
 
-        pwdcheckEt.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                if (!checkPasswordMatch()){
-                    pwd_check_warningTV.visibility = View.VISIBLE
-
-                } else {
-                    pwd_check_warningTV.visibility = View.GONE
-                    signupBtn.isEnabled = true
-                }
+        viewModel.isPasswordMatch.observe(this) { isPasswordMatch ->
+            pwd_check_warningTV.visibility = if (isPasswordMatch) {
+                View.GONE // 비밀번호가 강력한 경우 텍스트뷰 숨기기
+            } else {
+                View.VISIBLE // 비밀번호가 강력하지 않은 경우 텍스트뷰 표시
             }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
+        }
 
-        signupBtn.isEnabled = false
+        // EditText들의 텍스트가 변경될 때마다 ViewModel에 값을 업데이트
+        nameEt.addTextChangedListener {
+            viewModel.setName(it.toString())
+        }
+
+        idEt.addTextChangedListener {
+            viewModel.setId(it.toString())
+        }
+
+        pwdEt.addTextChangedListener {
+            viewModel.setPassword(it.toString())
+        }
+
+        pwdcheckEt.addTextChangedListener {
+            viewModel.setPasswordCheck(it.toString())
+        }
+
         signupBtn.setOnClickListener {
             signUpButtonClick()
         }
     }
 
     private fun signUpButtonClick() {
-        val name = nameEt.text.toString()
-        val id = idEt.text.toString()
-        val pwd = pwdEt.text.toString()
+        val name = viewModel.name.value
+        val id = viewModel.id.value
+        val pwd = viewModel.password.value
 
-        if (name.isNotEmpty() && id.isNotEmpty() && pwd.isNotEmpty()) {
-            viewModel.setUserValue(name, id, pwd)
-            Toast.makeText(this@SignUpActivity, "회원가입 성공\n이름: $name\n아이디: $id\n비밀번호: $pwd", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
-            intent.putExtra("id", id)
-            intent.putExtra("pwd", pwd)
-            // 이전 액티비티로 돌아가기 전에 현재 액티비티에서 처리한 결과를 담은 인텐트를 설정
-            setResult(RESULT_OK, intent)
-            finish()
-        } else {
-            Toast.makeText(this@SignUpActivity, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+        viewModel.updateSignUpButtonState()
+
+        viewModel.isSignUpEnabled.observe(this) { isSignUpEnabled ->
+            if (isSignUpEnabled) {
+                Toast.makeText(this@SignUpActivity, "회원가입 성공\n이름: $name\n아이디: $id\n비밀번호: $pwd", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+                intent.putExtra("id", id)
+                intent.putExtra("pwd", pwd)
+                // 이전 액티비티로 돌아가기 전에 현재 액티비티에서 처리한 결과를 담은 인텐트를 설정
+                setResult(RESULT_OK, intent)
+                finish()
+            } else {
+                Toast.makeText(this@SignUpActivity, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
-    }
-
-    private fun checkPasswordMatch(): Boolean {
-        val password = pwdEt.text.toString()
-        val passwordCheck = pwdcheckEt.text.toString()
-
-        return password == passwordCheck
     }
 }
